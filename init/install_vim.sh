0239e870dfb175a52dabc57b456ba7abc9a6fccd
Yuta Yamaguchi <yamaguchi7073xtt@gmail.com>
2017-12-16 17:55:37 +0900

    initial commit

---
+++ init/install_vim.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+brew update
+brew install lua
+brew install vim --with-lua
+brew install macvim
+brew link macvim
+
---
+++ init/install_zsh.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+brew update
+brew install zsh zsh-completions
