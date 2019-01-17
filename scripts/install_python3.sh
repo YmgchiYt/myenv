c1c1887920e8f0757ce031157304a8421193ee8b
ymgyt <yamaguchi7073xtt@gmail.com>
2019-01-17 18:45:59 +0900

    add pyenv config

--- scripts/install_python3.sh
+++ scripts/install_python3.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-function install_python3() (
-  brew install python3
+function install_pyenv (
+  brew install pyenv
+)
+
+function install_anacodna() (
+  # --verboseつけてinstall先を指定しないとだめかもしれない
+  pyenv install anaconda3-5.2.0
 )
 
 function install_sphinx() (
-  pip3 install Sphinx
-  pip3 install sphinx_rtd_theme
+  conda install sphinx
+  conda install sphinx_rtd_theme
 )
 
 function install_awscli() (
-  pip3 install awscli
+  conda install awscli
 )
 
-install_python3
+install_pyenv
+install_anaconda
 install_sphinx
 install_awscli
